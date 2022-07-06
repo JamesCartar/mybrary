51bcf7f2524d0aabab3faae111ead08db02475f8
heinhtet <hein08308@gmail.com>
2022-07-06 08:12:52 +0630

    initial setup

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
-const db = mongoose.connection;
-db.on("error", (error) => console.log(error));
-db.once("open", () => console.log("connect to mongoose"));
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+// const db = mongoose.connection;
+// db.on("error", (error) => console.log(error));
+// db.once("open", () => console.log("connect to mongoose"));
 
 app.use("/", indexRouter);
 
